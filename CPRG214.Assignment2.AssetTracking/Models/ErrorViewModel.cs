30e4dedb4514a800cf7ca5bb765fc1861a5a786b
ricobadico <biondiceric@gmail.com>
2021-02-13 20:37:44 -0700

    added mvc project

---
+++ CPRG214.Assignment2.AssetTracking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CPRG214.Assignment2.AssetTracking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
